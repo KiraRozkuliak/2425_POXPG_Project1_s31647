0e75de49519772c0acbc33271e4b8c5d3a6ff127
KiraRozkuliak <s31647@pjwstk.edu.pl>
2025-05-12 15:40:41 +0200

    Cw 12.05 kon

--- Assets/Scripts/GameControler.cs
+++ Assets/Scripts/GameControler.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
@@ -6,11 +6,69 @@ using UnityEngine;
 public class GameControler : MonoBehaviour
 {
     public string mainWord = "default";
+    public int livesCount = 10;
     public TextMeshProUGUI mainWordText;
+    public TextMeshProUGUI livesCountText;
+    public TextMeshProUGUI messegeText;
+    public TMP_InputField inputField;
     // Start is called before the first frame update
     void Start()
     {
         mainWordText.text = mainWord;
+        livesCountText.text = $"Zostało {livesCount} prób";
+    }
+
+    public void ONClick()
+    {
+        Debug.Log($"Guzzik został kliknięty");
+        livesCount = livesCount - 1;
+        livesCountText.text = $"Zostało {livesCount} prób";
+
+        if(mainWord == inputField.text)
+        {
+            messegeText.text = $"Poprawne słowo zostało wpisane";
+            return; // koniec metody
+        }
+
+        if (mainWord.Length != inputField.text.Length) 
+        {
+            messegeText.text = $"Liczba liter się nie zgadza";
+            return; // koniec metody
+        }
+
+         int countBulls = CountBulls();
+         int cowsBulls = CountCows();
+         messegeText.text = $"Bulls: {countBulls} and Cows: {cowsBulls}";
+
+    }
+
+
+    public int CountBulls()
+    {
+        int result = 0;
+
+        for 
+            (int i = 0; i < mainWord.Length; i++)
+        {
+            if (mainWord[i] == inputField.text[i])
+            {
+                result++;
+            }
+        }
+        return result;
+    }
+    public int CountCows()
+    {
+
+        int result = 0;
+
+        for
+            (int i = 0; i < mainWord.Length; i++)
+        {
+            if (mainWord[i] != inputField.text[i] && mainWord.Contains(inputField.text[i]) )
+                    result++;
+        }
+        return result;
     }
 
     // Update is called once per frame
